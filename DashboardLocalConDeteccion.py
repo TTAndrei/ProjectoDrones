4b2fa491a1b10dc3af51f6bfd26a5b9544dc141a
TTAndrei <andrei.akigu@gmail.com>
2026-02-26 11:54:17 +0100

    Cambio la cantidad de analisis de frame de 100 a 20, y añadido un boton para detectar bicicletas con la camara

--- DashboardLocalConDeteccion.py
+++ DashboardLocalConDeteccion.py
@@ -76,7 +76,7 @@ class VideoReceiver:
                     #print(f"Unexpected frame type: {type(frame)}")
                     continue
                 if self.objectID:
-                    if frame_count % 25 == 0:
+                    if frame_count % 20 == 0:
                         detectado, rectangulo  = self.detector.detect(frame,self.objectID)
 
                     if detectado:
@@ -266,6 +266,7 @@ def videoThread ():
 def video ():
     threading.Thread (target = videoThread).start()
 
+# Objetos
 def platano ():
     global video_receiver
     video_receiver.setObject(46)
@@ -276,6 +277,27 @@ def clock ():
 def pizza ():
     video_receiver.setObject(53)
 
+def bicicleta ():
+    video_receiver.setObject(1)
+
+'''
+0: person, 1: bicycle, 2: car, 3: motorcycle, 4: airplane, 5: bus,
+6: train, 7: truck, 8: boat, 9: traffic light, 10: fire hydrant,
+11: stop sign, 12: parking meter, 13: bench, 14: bird, 15: cat,
+16: dog, 17: horse, 18: sheep, 19: cow, 20: elephant, 21: bear,
+22: zebra, 23: giraffe, 24: backpack, 25: umbrella, 26: handbag,
+27: tie, 28: suitcase, 29: frisbee, 30: skis, 31: snowboard,
+32: sports ball, 33: kite, 34: baseball bat, 35: baseball glove,
+36: skateboard, 37: surfboard, 38: tennis racket, 39: bottle,
+40: wine glass, 41: cup, 42: fork, 43: knife, 44: spoon, 45: bowl,
+46: banana, 47: apple, 48: sandwich, 49: orange, 50: broccoli,
+51: carrot, 52: hot dog, 53: pizza, 54: donut, 55: cake,
+56: chair, 57: couch, 58: potted plant, 59: bed, 60: dining table,
+61: toilet, 62: tv, 63: laptop, 64: mouse, 65: remote,
+66: keyboard, 67: cell phone, 68: microwave, 69: oven, 70: toaster,
+71: sink, 72: refrigerator, 73: book, 74: clock, 75: vase,
+76: scissors, 77: teddy bear, 78: hair drier, 79: toothbrush
+'''
 def crear_ventana():
     global dron
     global  altShowLbl, headingShowLbl,  speedSldr, gradesSldr, stateShowLbl
@@ -433,6 +455,7 @@ def crear_ventana():
     detectFrame.columnconfigure(0, weight=1)
     detectFrame.columnconfigure(1, weight=1)
     detectFrame.columnconfigure(2, weight=1)
+    detectFrame.columnconfigure(3, weight=1)
 
     bananaBtn = tk.Button(detectFrame, text="Banana", bg="dark orange", command=platano)
     bananaBtn.grid(row=0, column=0, padx=5, pady=5, sticky=tk.N + tk.S + tk.E + tk.W)
@@ -442,6 +465,9 @@ def crear_ventana():
 
     pizzaBtn = tk.Button(detectFrame, text="Pizza", bg="dark orange", command=pizza)
     pizzaBtn.grid(row=0, column=2, padx=5, pady=5, sticky=tk.N + tk.S + tk.E + tk.W)
+
+    bicicletaBtn = tk.Button(detectFrame, text="Bicicleta", bg="dark orange", command=bicicleta)
+    bicicletaBtn.grid(row=0, column=3, padx=5, pady=5, sticky=tk.N + tk.S + tk.E + tk.W)
 
     return ventana
 
